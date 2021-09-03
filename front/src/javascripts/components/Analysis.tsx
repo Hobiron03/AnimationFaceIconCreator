f075df1cf38e65ebda94f1452aae9f24cd7ea84e
HOBIRON <yudainepnep@icloud.com>
2021-09-03 16:06:55 +0900

    feat:

--- front/src/javascripts/components/Analysis.tsx
+++ front/src/javascripts/components/Analysis.tsx
@@ -1,23 +1,34 @@
 import * as React from "React";
 import { useState, useEffect, useContext } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+
 import firebase from "../../../Firebase";
 
 interface ReivewData {
   name: string;
   dynamicFaceIcon: string;
   title: string;
+  movieTitle: string;
   canvasImage: string;
   EmotionalFaceIcon: Array<string>;
   comments: Array<string>;
   emotions: Array<string>;
 }
 
+const categories = [
+  ["がんこちゃん", "がんこちゃん2"],
+  ["空撮", "空撮2"],
+  ["花のワルツ", "新世界より"],
+];
+
 const Analysis = () => {
+  const classes = useStyles();
   const [reviews, setReviews] = useState([]);
   const [review, setReview] = useState<ReivewData>({
     name: "",
     dynamicFaceIcon: "",
     title: "",
+    movieTitle: "",
     canvasImage: "",
     EmotionalFaceIcon: [],
     comments: [],
@@ -34,24 +45,78 @@ const Analysis = () => {
     setReviews(reviews);
   };
 
+  const FetchReivewsB3 = async () => {
+    const snapshot = await firebase.firestore().collection("reviewsB3").get();
+    const reviews = snapshot.docs.map((doc) => doc.data());
+    setReviews(reviews);
+  };
+
+  const FilterReviewByMovieTitle = (movieTitle: string): Array<ReivewData> => {
+    return reviews.filter((review) => review.movieTitle === movieTitle);
+  };
+
+  const FilterReviewByMovieTitles = (
+    movieTitles: Array<string>
+  ): Array<ReivewData> => {
+    return reviews.filter((review) => movieTitles.includes(review.movieTitle));
+  };
+
+  const FilterReviewByCategory = (category) => {
+    return (
+      <div className={classes.separateByCategoryReview}>
+        {FilterReviewByMovieTitles(category).map(
+          (review: ReivewData, index: number) => {
+            return (
+              <div className={classes.result} key={index}>
+                <p>{review.name}</p>
+                <div className={classes.faceWithCoordinate}>
+                  <img src={review.dynamicFaceIcon} alt="" width={90} />
+                  <img src={review.canvasImage} alt="" width={120} />
+                </div>
+                <p>
+                  {review.emotions[0]} → {review.emotions[1]} →{" "}
+                  {review.emotions[2]} →{review.emotions[3]}
+                </p>
+              </div>
+            );
+          }
+        )}
+      </div>
+    );
+  };
+
   return (
     <div>
-      <p>Analysis Screen</p>
-      {reviews.map((review: ReivewData, index: number) => {
+      {categories.map((category) => {
         return (
           <div>
-            <p>{review.name}</p>
-            <p>{review.title}</p>
-            <img src={review.dynamicFaceIcon} alt="" />
-            <img src={review.canvasImage} alt="" width={200} />
-            <p>
-              {review.emotions[0]} → {review.emotions[1]} →{review.emotions[3]}
-            </p>
+            <h3>{category}</h3>
+            {FilterReviewByCategory(category)}
           </div>
         );
       })}
     </div>
   );
 };
+
+const useStyles = makeStyles((theme) => ({
+  coordinateArea: {
+    margin: "0 auto",
+    width: 420,
+  },
+  result: {
+    margin: 10,
+    width: 300,
+    cursor: "pointer",
+    border: "1px solid gray",
+    borderRadius: "10%",
+    textAlign: "center",
+  },
+  faceWithCoordinate: {},
+  separateByCategoryReview: {
+    display: "flex",
+    flexWrap: "wrap",
+  },
+}));
 
 export default Analysis;
