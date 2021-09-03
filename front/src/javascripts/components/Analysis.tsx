18bfc4f5bd36550ea7273a861518df67c9ba7de7
HOBIRON <yudainepnep@icloud.com>
2021-09-03 16:48:25 +0900

    feat:

--- front/src/javascripts/components/Analysis.tsx
+++ front/src/javascripts/components/Analysis.tsx
@@ -1,6 +1,7 @@
 import * as React from "React";
 import { useState, useEffect, useContext } from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
 
 import firebase from "../../../Firebase";
 
@@ -35,6 +36,8 @@ const Analysis = () => {
     emotions: [],
   });
 
+  const [isOpenReviewModal, setIsOpenReviewModal] = useState(false);
+
   useEffect(() => {
     FetchReivews();
   }, []);
@@ -61,13 +64,73 @@ const Analysis = () => {
     return reviews.filter((review) => movieTitles.includes(review.movieTitle));
   };
 
+  const CloseReviewModal = () => {
+    setIsOpenReviewModal(false);
+  };
+
+  const OpenReviewModal = (review) => {
+    setReview(review);
+    setIsOpenReviewModal(true);
+  };
+
+  const ReivewContent = () => {
+    return (
+      <div>
+        <div className="c-modal_content_inner__header">
+          <div className="c-modal_content_inner__header__faceicon">
+            <img src={review.dynamicFaceIcon} alt="" />
+          </div>
+          <div className="c-modal_content_inner__header__title">
+            <h2>{review.title}</h2>
+          </div>
+        </div>
+
+        {/* レビューの詳細かな */}
+        {review.EmotionalFaceIcon.map((faceIcon, index) => {
+          return (
+            <div className="c-modal_content_inner__content">
+              <div className="c-modal_content_inner__content__columns">
+                <div className="c-modal_content_inner__content__columns__faceicon">
+                  <img src={faceIcon} alt="" />
+                </div>
+                <div className="c-modal_content_inner__content__columns__comment">
+                  {/* index = 0はタイトルの文章が入っている */}
+                  <p>{review.comments[index + 1]}</p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  };
+
+  const ReivewModal = () => {
+    if (isOpenReviewModal) {
+      return (
+        <Modal
+          open={isOpenReviewModal}
+          onClose={CloseReviewModal}
+          aria-labelledby="simple-modal-title"
+          aria-describedby="simple-modal-description"
+        >
+          <div className={classes.paper}>{ReivewContent()}</div>
+        </Modal>
+      );
+    }
+  };
+
   const FilterReviewByCategory = (category) => {
     return (
       <div className={classes.separateByCategoryReview}>
         {FilterReviewByMovieTitles(category).map(
           (review: ReivewData, index: number) => {
             return (
-              <div className={classes.result} key={index}>
+              <div
+                className={classes.result}
+                key={index}
+                onClick={() => OpenReviewModal(review)}
+              >
                 <p>{review.name}</p>
                 <div className={classes.faceWithCoordinate}>
                   <img src={review.dynamicFaceIcon} alt="" width={90} />
@@ -87,9 +150,10 @@ const Analysis = () => {
 
   return (
     <div>
+      {ReivewModal()}
       {categories.map((category) => {
         return (
-          <div>
+          <div className={classes.area}>
             <h3>{category}</h3>
             {FilterReviewByCategory(category)}
           </div>
@@ -100,17 +164,29 @@ const Analysis = () => {
 };
 
 const useStyles = makeStyles((theme) => ({
-  coordinateArea: {
-    margin: "0 auto",
-    width: 420,
+  paper: {
+    width: 1200,
+    margin: "30px auto",
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
   },
   result: {
     margin: 10,
     width: 300,
     cursor: "pointer",
-    border: "1px solid gray",
+    border: "0.5px solid gray",
     borderRadius: "10%",
     textAlign: "center",
+    transition: "all 0.2s",
+    boxShadow: "0 5px 10px 0 rgba(0, 0, 0, 0.2)",
+    "&:hover": {
+      transform: "scale(1.03, 1.03)",
+    },
+  },
+  area: {
+    marginBottom: 100,
   },
   faceWithCoordinate: {},
   separateByCategoryReview: {
